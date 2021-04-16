d74609d8ba142c13d4697f7e4eb6b8746cbd1be2
whatseven <1787609016@qq.com>
2021-04-16 18:33:39 +0800

    1

--- src/3d_detection/model.py
+++ src/3d_detection/model.py
@@ -1,4 +1,5 @@
 import torch
+from easydict import EasyDict
 from torchvision.models import resnet101
 from torch import nn
 import numpy as np
@@ -18,42 +19,6 @@ class Yolo3D(nn.Module):
     def __init__(self, v_cfg):
         super(Yolo3D, self).__init__()
         det_3d_cfg = cfg_from_file(v_cfg["det_3d"]["visualDet3D_config_path"])
-        if v_cfg["det_3d"]["with_anchor"]==True:
-            anchor_manager = Anchors(
-                "temp/anchors",
-                readConfigFile=False,
-                pyramid_levels=[4],
-                strides=[16],
-                sizes=[24],
-                ratios=[.5,1.],
-                scales=np.array([2 ** (i / 4.0) for i in range(16)]),
-                obj_types=det_3d_cfg.obj_types,
-                filter_anchors=True,
-                filter_y_threshold_min_max=None,
-                filter_x_threshold=None,
-                anchor_prior_channel=6,)
-            preprocess = build_augmentator(det_3d_cfg.data.test_augmentation)
-
-            train_index_names=[item.strip() for item in open("temp/train_split.txt").readlines()]
-            for i, index_name in enumerate(train_index_names):
-                data_frame = KittiData(det_3d_cfg.path.data_path, index_name, {
-                                "calib": True,
-                                "image": True,
-                                "label": True,
-                                "velodyne": False,
-                            })
-                calib, image, label, velo = data_frame.read_data()
-                for j in range(len(det_3d_cfg.obj_types)):
-                    total_objects[j] += len([obj for obj in data_frame.label if obj.type==cfg.obj_types[j]])
-                    data = np.array(
-                        [
-                            [obj.z, np.sin(2 * obj.alpha), np.cos(2 * obj.alpha), obj.w, obj.h, obj.l]
-                                for obj in data_frame.label if obj.type==cfg.obj_types[j]
-                        ]
-                    ) #[N, 6]
-                    if data.any():
-                        uniform_sum_each_type[j, :] += np.sum(data, axis=0)
-                        uniform_square_each_type[j, :] += np.sum(data ** 2, axis=0)
 
         self.bbox_head = AnchorBasedDetection3DHead(num_features_in=1024,
                                                     num_classes=3,
@@ -64,6 +29,80 @@ class Yolo3D(nn.Module):
                                                     loss_cfg=EasyDict(),
                                                     test_cfg=EasyDict(),
                                                     read_precompute_anchor=True)
+
+        if v_cfg["det_3d"]["with_anchor"]==True:
+            anchor_manager = Anchors(
+                "temp/anchors",
+                readConfigFile=False,
+                pyramid_levels=[4],
+                strides=[16],
+                sizes=[24],
+                ratios=[.5,1.],
+                scales=np.array([2 ** (i / 4.0) for i in range(16)]),
+                obj_types=['Car'],
+                filter_anchors=True,
+                filter_y_threshold_min_max=None,
+                filter_x_threshold=None,
+                anchor_prior_channel=6,)
+            # Get the training split
+            train_index_names=[item.strip() for item in open("temp/train_split.txt").readlines()]
+
+            # Statistics about the anchor
+            total_objects = 0
+            total_usable_objects = 0
+            uniform_sum_each_type = []
+            uniform_square_each_type = []
+            len_scale = len(anchor_manager.scales)
+            len_ratios = len(anchor_manager.ratios)
+            len_level = len(anchor_manager.pyramid_levels)
+            examine = [list() for i in range(len_level * len_scale)]
+            sums = [list() for i in range(len_level * len_scale)]
+            squared = [list() for i in range(len_level * len_scale)]
+            for i, index_name in enumerate(train_index_names):
+                data_frame = KittiData(det_3d_cfg.path.data_path, index_name, {
+                                "calib": True,
+                                "image": True,
+                                "label": True,
+                                "velodyne": False,
+                            })
+                calib, image, label, velo = data_frame.read_data()
+                data_frame.calib = calib
+                # Replace the 2d bounding box with re-projection
+                data_frame.bbox2d=None
+
+                total_objects += len(data_frame.label)
+                data = np.array(
+                    [
+                        [obj.x, obj.y, obj.z, obj.w, obj.h, obj.l,obj.theta]for obj in data_frame.label
+                    ]
+                )
+                if data.any():
+                    uniform_sum_each_type.append(np.sum(data, axis=0))
+                    uniform_square_each_type.append(np.sum(data ** 2, axis=0))
+
+                anchors, _ = anchor_manager(image[np.newaxis].transpose([0,3,1,2]), torch.tensor(calib.P2).reshape([-1, 3, 4]))
+                bbox3d = torch.tensor([[obj.x, obj.y, obj.z, obj.theta] for obj in label]).cuda()
+                usable_anchors = anchors[0]
+
+                IoUs = calc_iou(usable_anchors, data_frame.bbox2d)  # [N, K]
+                IoU_max, IoU_argmax = torch.max(IoUs, dim=0)
+                IoU_max_anchor, IoU_argmax_anchor = torch.max(IoUs, dim=1)
+
+                num_usable_object = torch.sum(IoU_max > cfg.detector.head.loss_cfg.fg_iou_threshold).item()
+                total_usable_objects[j] += num_usable_object
+
+                positive_anchors_mask = IoU_max_anchor > cfg.detector.head.loss_cfg.fg_iou_threshold
+                positive_ground_truth_3d = bbox3d[IoU_argmax_anchor[positive_anchors_mask]].cpu().numpy()
+
+                used_anchors = usable_anchors[positive_anchors_mask].cpu().numpy()  # [x1, y1, x2, y2]
+
+                sizes_int, ratio_int = anchor_manager.anchors2indexes(used_anchors)
+                for k in range(len(sizes_int)):
+                    examine[sizes_int[k], ratio_int[k]] += 1
+                    sums[sizes_int[k], ratio_int[k]] += positive_ground_truth_3d[k, 2:5]
+                    squared[sizes_int[k], ratio_int[k]] += positive_ground_truth_3d[k, 2:5] ** 2
+
+
         self.core = YoloMono3DCore()
         self.network_cfg = det_3d_cfg
         self.obj_types = det_3d_cfg.obj_types

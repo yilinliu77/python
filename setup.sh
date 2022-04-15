917121ffd7960354f34cd93e674cebafae61b6f3
whatseven <1787609016@qq.com>
2022-04-15 21:45:46 +0800

    update for docker

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ function with_backoff {
   return $exitCode
 }
 
-source ~/.bashrc
+source ~/.bashrc && ~/miniconda/bin/conda activate base
 
 conda install -y opencv pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
 

5ba32140d658229679f11a4b5345b2c318160e25
whatseven <1787609016@qq.com>
2022-04-19 13:01:51 +0800

    remove sigmoid in spearman loss

--- src/regress_reconstructability_hyper_parameters/model.py
+++ src/regress_reconstructability_hyper_parameters/model.py
@@ -2435,7 +2435,7 @@ class Uncertainty_Modeling_wo_pointnet18(Uncertainty_Modeling_wo_pointnet8):
         self.view_feature_extractor = nn.Sequential(
             nn.Linear(5, 128),
         )
-        self.view_feature_fusioner1 = TFEncorder(128, 1, 128, 0.0, batch_first=True, add_bias_kv=self.hydra_conf["model"]["add_bias_kv"])
+        self.view_feature_fusioner1 = TFEncorder(128, 1, 128, 0.2, batch_first=True, add_bias_kv=self.hydra_conf["model"]["add_bias_kv"])
 
         self.features_to_recon_error = nn.Sequential(
             nn.Linear(128, 1),

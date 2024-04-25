a3782febedfc0335989665c3797d850d291c4d43
yilinliu77 <whatsevenlyl@gmail.com>
2024-04-25 16:35:37 +0800

    fix 768 version

--- src/img2brep/brep/model_fuser.py
+++ src/img2brep/brep/model_fuser.py
@@ -9,8 +9,6 @@ class Attn_fuser_cross(nn.Module):
         self.atten = nn.ModuleList([
             nn.TransformerDecoderLayer(dim, 4, dim_feedforward=dim, dropout=0.1, batch_first=True),
             nn.TransformerDecoderLayer(dim, 4, dim_feedforward=dim, dropout=0.1, batch_first=True),
-            nn.TransformerDecoderLayer(dim, 4, dim_feedforward=dim, dropout=0.1, batch_first=True),
-            nn.TransformerDecoderLayer(dim, 4, dim_feedforward=dim, dropout=0.1, batch_first=True),
         ])
         pass
 
@@ -40,8 +38,6 @@ class Attn_fuser_single(nn.Module):
         super().__init__()
         hidden_dim = dim
         self.atten = nn.ModuleList([
-            nn.TransformerEncoderLayer(hidden_dim, 4, dim_feedforward=hidden_dim, dropout=0.1, batch_first=True),
-            nn.TransformerEncoderLayer(hidden_dim, 4, dim_feedforward=hidden_dim, dropout=0.1, batch_first=True),
             nn.TransformerEncoderLayer(hidden_dim, 4, dim_feedforward=hidden_dim, dropout=0.1, batch_first=True),
             nn.TransformerEncoderLayer(hidden_dim, 4, dim_feedforward=hidden_dim, dropout=0.1, batch_first=True),
         ])

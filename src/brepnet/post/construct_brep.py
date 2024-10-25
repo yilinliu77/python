9a6cb0068808de633c3831938275eae11b1ada09
yilinliu77 <whatsevenlyl@gmail.com>
2024-10-25 17:46:54 +0800

    small bug fix in post processing

--- src/brepnet/post/construct_brep.py
+++ src/brepnet/post/construct_brep.py
@@ -322,7 +322,7 @@ def construct_brep_from_datanpz_batch(data_root, out_root, folder_name_list,
             return None
 
 
-construct_brep_from_datanpz_batch_ray = ray.remote(max_retries=2, num_cpus=1, num_gpus=0.01)(construct_brep_from_datanpz_batch)
+construct_brep_from_datanpz_batch_ray = ray.remote(max_retries=2, num_cpus=1, num_gpus=0.1)(construct_brep_from_datanpz_batch)
 
 
 def test_construct_brep(v_data_root, v_out_root, v_prefix, use_cuda, from_scratch):

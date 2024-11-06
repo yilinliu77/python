cdea90f9d9007600eeac0815331820af00462796
xuduoteng <1820331297@qq.com>
2024-11-06 18:01:47 +0800

    post_update

--- src/brepnet/eval/lfd/evaluation_scripts/load_data/3DAlignment/Circularity.h
+++
@@ -1,1 +0,0 @@
-double Circularity(unsigned char *srcBuff, int width, int height, unsigned char *edge);
--- src/brepnet/eval/lfd/evaluation_scripts/load_data/3DAlignment/RecovAffine.h
+++
@@ -1,1 +0,0 @@
-double RecoverAffine(double **matrix, double cost[ANGLE][ANGLE][CAMNUM_2][CAMNUM_2], int *MinSrcCam);

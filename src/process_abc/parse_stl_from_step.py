f410d032b6ec7ddbb24d57f479d57b2face26cf0
xuduoteng <1820331297@qq.com>
2024-11-21 12:19:59 +0800

    save1

--- src/process_abc/parse_stl_from_step.py
+++ src/process_abc/parse_stl_from_step.py
@@ -12,20 +12,22 @@ import argparse
 # out_root = r"E:\data\img2brep\test"
 
 
-def step2stl(step_folder, output_folder):
-    try:
-        step_files = glob.glob(os.path.join(step_folder, "*.step"))
-        if len(step_files) == 0:
-            return
-        step_file = step_files[0]
-        stl_file = os.path.join(output_folder, "mesh.stl")
-        shape = read_step_file(step_file)
-        os.makedirs(output_folder, exist_ok=True)
-        write_stl_file(shape, stl_file, linear_deflection=0.01, angular_deflection=0.5)
-    except Exception as e:
-        if os.path.exists(output_folder):
-            shutil.rmtree(output_folder)
-        pass
+def step2stl(data_root, step_folders, output_root):
+    for step_folder in step_folders:
+        output_folder = str(os.path.join(output_root, os.path.basename(step_folder)))
+        try:
+            step_files = glob.glob(os.path.join(data_root, step_folder, "*.step"))
+            if len(step_files) == 0:
+                continue
+            step_file = step_files[0]
+            shape = read_step_file(step_file, as_compound=False, verbosity=False)
+            stl_file = os.path.join(output_folder, "mesh.stl")
+            os.makedirs(output_folder, exist_ok=True)
+            write_stl_file(shape, stl_file, linear_deflection=0.01, angular_deflection=0.5)
+        except Exception as e:
+            if os.path.exists(output_folder):
+                shutil.rmtree(output_folder)
+            pass
 
 
 step2stl_remote = ray.remote(step2stl)
@@ -44,17 +46,17 @@ if __name__ == "__main__":
         os.makedirs(out_root)
 
     all_folders = os.listdir(data_root)
-    for folder in tqdm(all_folders):
-        step_folder = os.path.join(data_root, folder)
-        output_folder = os.path.join(out_root, folder)
-        step2stl(step_folder, output_folder)
-
-    # ray.init()
-    # futures = []
     # for folder in tqdm(all_folders):
     #     step_folder = os.path.join(data_root, folder)
     #     output_folder = os.path.join(out_root, folder)
-    #     futures.append(step2stl_remote.remote(step_folder, output_folder))
-    # for f in tqdm(futures):
-    #     ray.get(f)
-    # ray.shutdown()
+    #     step2stl(step_folder, output_folder)
+
+    ray.init(local_mode=True)
+    futures = []
+    batch_size = 10000
+    for i in range(0, len(all_folders), batch_size):
+        step_folders = all_folders[i:i + batch_size]
+        futures.append(step2stl_remote.remote(data_root, step_folders, out_root))
+    for f in tqdm(futures):
+        ray.get(f)
+    ray.shutdown()

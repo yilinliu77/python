a6e4469c5014d0ae5e5d4b4892457863fe0a979f
xuduoteng <1820331297@qq.com>
2024-11-26 15:27:08 +0800

    xdt_update

--- src/brepnet/data/check_data_from_lfd.py
+++ src/brepnet/data/check_data_from_lfd.py
@@ -5,58 +5,94 @@ import numpy as np
 from tqdm import tqdm
 
 from src.brepnet.viz.sort_and_merge import arrange_meshes
+import ray
+
+
+def process_pkl(pkl_file):
+    with open(pkl_file, "rb") as f:
+        data = pickle.load(f)
+    local_folder_list = data[0]
+    local_folder_list = [os.path.basename(item) for item in local_folder_list]
+    local_removed_folder_list = []
+    local_unique_idx_list = list(range(len(local_folder_list)))
+    local_lfd_matrix = data[1]
+    idx0, idx1 = np.where(local_lfd_matrix < lfd_threshold)
+    for i, j in list(zip(idx0, idx1)):
+        if i < j:
+            if j in local_unique_idx_list:
+                local_unique_idx_list.remove(j)
+                local_removed_folder_list.append((local_folder_list[i], local_folder_list[j]))
+    local_unique_folder_list = [local_folder_list[i] for i in local_unique_idx_list]
+    print(f"Pkl file path: {os.path.basename(pkl_file)}, Local Unique ratio: {len(local_unique_folder_list) / len(local_folder_list)}")
+    return local_folder_list, local_unique_folder_list, local_removed_folder_list
+
+
+process_pkl_remote = ray.remote(process_pkl)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Construct Brep From Data')
     parser.add_argument('--pkl_root', type=str, required=True)
     parser.add_argument('--lfd', type=float, required=False, default=10)
+    parser.add_argument("--use_ray", action='store_true')
     args = parser.parse_args()
     pkl_root = args.pkl_root
     lfd_threshold = args.lfd
-    out_txt_path = os.path.join(pkl_root, f"brepgen_train_novel_lfd_{lfd_threshold}.txt")
+    out_txt_path = pkl_root + f"brepgen_train_novel_lfd_{lfd_threshold}.txt"
 
     all_pkl_files = os.listdir(pkl_root)
+    # all_pkl_files = all_pkl_files[0:10]
     folder_list = []
     unique_folder_list = []
     removed_folder_list = []
-    for pkl_file in tqdm(all_pkl_files):
-        if not pkl_file.endswith(".pkl"):
-            continue
-        print(f"Processing {pkl_file}")
-        with open(os.path.join(pkl_root, pkl_file), "rb") as f:
-            data = pickle.load(f)
-        local_folder_list = data[0]
-        local_unique_idx_list = list(range(len(local_folder_list)))
-        local_lfd_matrix = data[1]
-        idx0, idx1 = np.where(local_lfd_matrix < lfd_threshold)
-        for i, j in tqdm(list(zip(idx0, idx1))):
-            if i < j:
-                if j in local_unique_idx_list:
-                    local_unique_idx_list.remove(j)
-                    removed_folder_list.append(local_folder_list[i])
-                    removed_folder_list.append(local_folder_list[j])
 
-        local_unique_folder_list = [local_folder_list[i] for i in local_unique_idx_list]
-        print(f"Local Unique ratio: {len(local_unique_folder_list) / len(local_folder_list)}")
-        folder_list.extend(local_folder_list)
-        unique_folder_list.extend(local_unique_folder_list)
+    if not args.use_ray:
+        for pkl_file in tqdm(all_pkl_files):
+            if not pkl_file.endswith(".pkl"):
+                continue
+            print(f"Processing {pkl_file}")
+            with open(os.path.join(pkl_root, pkl_file), "rb") as f:
+                data = pickle.load(f)
+            local_folder_list = data[0]
+            local_folder_list = [os.path.basename(item) for item in local_folder_list]
+            local_unique_idx_list = list(range(len(local_folder_list)))
+            local_lfd_matrix = data[1]
+            idx0, idx1 = np.where(local_lfd_matrix < lfd_threshold)
+            for i, j in tqdm(list(zip(idx0, idx1))):
+                if i < j:
+                    if j in local_unique_idx_list:
+                        local_unique_idx_list.remove(j)
+                        removed_folder_list.append((local_folder_list[i], local_folder_list[j]))
+            local_unique_folder_list = [local_folder_list[i] for i in local_unique_idx_list]
+            print(f"Local Unique ratio: {len(local_unique_folder_list) / len(local_folder_list)}")
+            folder_list.extend(local_folder_list)
+            unique_folder_list.extend(local_unique_folder_list)
+    else:
+        ray.init(local_mode=False)
+        futures = []
+        for pkl_file in tqdm(all_pkl_files):
+            if not pkl_file.endswith(".pkl"):
+                continue
+            futures.append(process_pkl_remote.remote(os.path.join(pkl_root, pkl_file)))
+        for future in tqdm(futures):
+            local_folder_list, local_unique_folder_list, local_removed_folder_list = ray.get(future)
+            folder_list.extend(local_folder_list)
+            unique_folder_list.extend(local_unique_folder_list)
+            removed_folder_list.extend(local_removed_folder_list)
 
     unique_folder_list.sort()
     print(f"Unique ratio: {len(unique_folder_list) / len(folder_list)}")
 
     # Sample deduplicated
-    existing_items = set(os.listdir("D:/brepnet/deepcad_v6"))
-    unique_folder_list = list(set(unique_folder_list) & existing_items)
-    unique_folder_list = np.random.choice(unique_folder_list, 2500, replace=False)
-    unique_folder_list = [os.path.join("D:/brepnet/deepcad_v6",item,"mesh.ply") for item in unique_folder_list]
-    arrange_meshes(unique_folder_list, "D:/brepnet/deepcad_v6/test.ply")
+    # existing_items = set(os.listdir("D:/brepnet/deepcad_v6"))
+    # unique_folder_list = list(set(unique_folder_list) & existing_items)
+    # unique_folder_list = np.random.choice(unique_folder_list, 2500, replace=False)
+    # unique_folder_list = [os.path.join("D:/brepnet/deepcad_v6", item, "mesh.ply") for item in unique_folder_list]
+    # arrange_meshes(unique_folder_list, "D:/brepnet/deepcad_v6/test.ply")
 
-    # Sample duplicated
-    unique_folder_list = list(set(unique_folder_list) & existing_items)
-    unique_folder_list = np.random.choice(unique_folder_list, 2500, replace=False)
-    unique_folder_list = [os.path.join("D:/brepnet/deepcad_v6",item,"mesh.ply") for item in unique_folder_list]
-    arrange_meshes(unique_folder_list, "D:/brepnet/deepcad_v6/test.ply")
-
+    deduplicated_txt_path = pkl_root + f"brepgen_train_deduplicated_lfd_{lfd_threshold}.txt"
+    with open(deduplicated_txt_path, "w") as f:
+        for folder in removed_folder_list:
+            f.write(f"{folder[0]} {folder[1]}\n")
 
     with open(out_txt_path, "w") as f:
         for folder in unique_folder_list:
